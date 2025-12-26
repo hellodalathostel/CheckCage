62a74423fe5bd04ecec1de6723dd14da9678b8f1
Su <hellodalathostel@gmail.com>
2025-12-26 10:31:58 +0700

    Fix Firebase credentials using base64

--- firebase.py
+++ firebase.py
@@ -1,11 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import base64
 import json
 import os
 
-cred = credentials.Certificate(
-    json.loads(os.getenv("FIREBASE_CREDENTIALS_JSON"))
-)
+encoded = os.getenv("FIREBASE_CREDENTIALS_BASE64")
+decoded = base64.b64decode(encoded).decode("utf-8")
+
+cred = credentials.Certificate(json.loads(decoded))
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
